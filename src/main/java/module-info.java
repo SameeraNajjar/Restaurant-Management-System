7799e34f989d28f961093a260df68937c863d8c0
SameeraNajjar <116748280+SameeraNajjar@users.noreply.github.com>
2024-12-11 20:52:11 +0200

    initiate the repo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.rmsproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.rmsproject to javafx.fxml;
+    exports org.example.rmsproject;
+}
